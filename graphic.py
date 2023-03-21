d5c55a04ce16fd6f17682f439a1ad88d3a2b5816
TheTrebuchet <jan.kulczycki1@gmail.com>
2023-03-21 20:49:12 +0100

    interpreting config just fine

--- graphic.py
+++ graphic.py
@@ -26,7 +26,7 @@ def delta(typ, mass, height, name, coords, tang, skew):
     for i in range(len(verts)-1):
         plt.plot((verts[i][0],verts[i+1][0]), (verts[i][1],verts[i+1][1]), c='black')
     plt.plot((verts[0][0],verts[-1][0]), (verts[0][1],verts[-1][1]), c='black')
-    ax.annotate(name + '\n'+str(round(mass,2)*2) + 'g/h', xy, ha='center', va='center').draggable()
+    ax.annotate(name + '\n'+str(round(mass,2)) + 'g/h', xy, ha='center', va='center').draggable()
 
 w, h = 10, 10
 fig = plt.figure(frameon=True)
@@ -35,51 +35,58 @@ ax = plt.Axes(fig, [0., 0., 1., 1.])
 ax.set_axis_off()
 fig.add_axes(ax)
 
-def execute(file):
+def draw(file):
 
     with open(file) as content:
         lines = content.readlines()
     lines = [i.replace('\n','') for i in lines if i != '\n']
+    print(lines)
     rows = []
     mode = 0
     for l in lines:
-        if '#' in l and '##' not in l:
-            rows.append([l[-1]])
+        if '    #' in l:
+            mode+=1
+        if mode and '    ' not in l:
+            print('indent',mode)
+            rows[-(mode+1)].append(rows[-mode:])
+            rows = rows[:-(mode)]
             mode = 0
-        elif '##' in l:
-            rows[-1].append([l[-1]])
-            mode = 1
-        elif mode == 0:rows[-1].append(l.split(' ',1))
-        elif mode == 1:rows[-1][-1].append(l.split(' ',1))
+        if '#'in l:
+            rows.append([l.strip()[1:]])
+        else:
+            rows[-1].append(l.strip().split(' ',1))
+    rows = [r for r in rows if r!=['']]
 
     L_height = 0.2
-    s_height = 0.06
-
+    s_height = 0.1
     skew = (750)/L_height*0.05
     tang = 1.5*L_height/(750)
-
     coords = [0,0]
-    
     mode = {'+':1,'=':0,'-':-1}
     height = {'L':L_height,'s':s_height}
-    skew_mode = 0
-    for row in rows:
-        h = height[row[0]]
-        print(max(row, key=len))
-        complexity = len(max(row, key=len))
-        if complexity==2:
-            h = sum([height[l] for l in max(row, key=len)])
-        mass = 0
-        print(row)
-        print(h)
-        for block in row[1:]:
-            print(float(block[0][1:]))
-            delta(mode[block[0][0]], float(block[0][1:]), h, block[1], [coords[0]+mass,coords[1]], tang,skew_mode)
-            mass+=float(block[0][1:])
-        coords[1]-=h
-        skew_mode = skew
+    def rowbyrow(rows,coords):
+        for row in rows:
+            h = sum(height[i] for i in row[0])
+            mass = 0
+            out = 0
+            ind = rows.index(row)
+            if ind == 0 or ind == len(rows)-1:s = 0
+            else:s=skew
+            print(row)
+            print(h)
+            for block in row[1:]:
+                if not isinstance(block[0],str):
+                    rowbyrow(block,[coords[0]+mass,coords[1]])
+                    continue
+                print(float(block[0][1:]))
+                delta(mode[block[0][0]], float(block[0][1:]), h, block[1], [coords[0]+mass,coords[1]], tang,s)
+                mass+=float(block[0][1:])
+                if block[0][0]=='-':out+=float(block[0][1:])
+            coords[1]-=h
+            coords[0]+=out
+    rowbyrow(rows,coords)
     plt.show()
-execute(file)
+draw(file)
 
 w, h = 10, 10
 fig = plt.figure(frameon=True)
