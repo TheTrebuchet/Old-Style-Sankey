e78421f9ebd87cc66a715e4d0406837449fdda97
TheTrebuchet <jan.kulczycki1@gmail.com>
2023-03-22 23:43:12 +0100

    units now work

--- graphic.py
+++ graphic.py
@@ -1,11 +1,29 @@
 import matplotlib.pyplot as plt
 from numpy import sign
-import sys
+import argparse
  
-file = str(sys.argv[1])
-if file == None:
-    print('you need to specify config.txt file')
+parser = argparse.ArgumentParser(
+                    prog='ClassicSankeyGen',
+                    description='Creates Sankey diagrams',
+                    epilog='Just read readme if you feel lost')
+
+parser.add_argument('filename')           # positional argument
+parser.add_argument('-u', '--unit')      # option that takes a value
+parser.add_argument('-g', '--grid', action='store_true')  # on/off flag
+args = parser.parse_args()
+
+if not args.unit:
+    print('you need to specify the unit\nadd for example \"-u g/h\"')
     quit()
+
+class block:
+    def __init__(self, line):
+        mode = {'+':1,'=':0,'-':-1}
+        self.delta = mode[line[0]]
+        self.value = float(line.split(' ',1)[0][1:])
+        self.name = line.split(' ',1)[1]
+
+
 
 def delta(typ, mass, height, name, coords, tang, skew):
     insert = tang*mass/2
@@ -26,7 +44,7 @@ def delta(typ, mass, height, name, coords, tang, skew):
     for i in range(len(verts)-1):
         plt.plot((verts[i][0],verts[i+1][0]), (verts[i][1],verts[i+1][1]), c='black')
     plt.plot((verts[0][0],verts[-1][0]), (verts[0][1],verts[-1][1]), c='black')
-    ax.annotate(name + '\n'+str(round(mass,2)) + 'g/h', xy, ha='center', va='center').draggable()
+    ax.annotate(name + '\n'+str(round(mass,2)) + args.unit, xy, ha='center', va='center').draggable()
 
 w, h = 10, 10
 fig = plt.figure(frameon=True)
@@ -40,23 +58,22 @@ def draw(file):
     with open(file) as content:
         lines = content.readlines()
     lines = [i.replace('\n','') for i in lines if i != '\n']
-    print(lines)
     rows = []
     mode = 0
     for l in lines:
         if '    #' in l:
             mode+=1
         if mode and '    ' not in l:
-            print('indent',mode)
             rows[-(mode+1)].append(rows[-mode:])
             rows = rows[:-(mode)]
             mode = 0
         if '#'in l:
             rows.append([l.strip()[1:]])
         else:
-            rows[-1].append(l.strip().split(' ',1))
+            rows[-1].append(block(l.strip()))
     rows = [r for r in rows if r!=['']]
 
+    #settings
     L_height = 0.2
     s_height = 0.1
     skew = (750)/L_height*0.05
@@ -64,6 +81,7 @@ def draw(file):
     coords = [0,0]
     mode = {'+':1,'=':0,'-':-1}
     height = {'L':L_height,'s':s_height}
+
     def rowbyrow(rows,coords):
         for row in rows:
             h = sum(height[i] for i in row[0])
@@ -72,19 +90,16 @@ def draw(file):
             ind = rows.index(row)
             if ind == 0 or ind == len(rows)-1:s = 0
             else:s=skew
-            print(row)
-            print(h)
-            for block in row[1:]:
-                if not isinstance(block[0],str):
-                    rowbyrow(block,[coords[0]+mass,coords[1]])
+            for entry in row[1:]:
+                if not isinstance(entry,block):
+                    rowbyrow(entry,[coords[0]+mass,coords[1]])
                     continue
-                print(float(block[0][1:]))
-                delta(mode[block[0][0]], float(block[0][1:]), h, block[1], [coords[0]+mass,coords[1]], tang,s)
-                mass+=float(block[0][1:])
-                if block[0][0]=='-':out+=float(block[0][1:])
+                delta(entry.delta, entry.value, h, entry.name, [coords[0]+mass,coords[1]], tang,s)
+                mass+=float(entry.value)
+                if entry.delta==-1:out+=float(entry.value)
             coords[1]-=h
             coords[0]+=out
     rowbyrow(rows,coords)
     plt.show()
 
-draw(file)
+draw(args.filename)
